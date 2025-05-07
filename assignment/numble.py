3abd9b524592d5cbcc7a76b39e5bb486cc64a329
1lent <brodiecuff@outlook.com>
2025-05-07 09:04:36 +0800

    added hints

--- assignment/numble.py
+++ assignment/numble.py
@@ -3,6 +3,15 @@ import random
 wins = 0  # total wins and stuff
 losses = 0
 money = 1000
+hints = False
+
+"""Addition for the assignment to add hints, in checking_guess() it will put an input saying do u want hints, if so it will call this once it sets hints = True"""
+
+
+def get_hints(random_num):
+    global hints
+    if hints:
+        print(f"The number {random.choice(random_num[1])} is in the numble")
 
 
 def get_random_number():
@@ -14,17 +23,19 @@ def get_random_number():
     random_num = random.choice(numbers)
     return random_num
 
+
+
 def get_user_number():
     user_number = input("Enter a 5 digit number to guess the numble: ")
     if len(user_number) != 5:
-        print("try again")
+        print("\ntry again\n5 Digits")
         return get_user_number()
     else:
         return user_number
 
 def get_user_money():
     global money
-    print("Your current amount of money is"  , money)
+    print("\nYour current amount of money is"  , money, "\n")
     inputMoney = input("Enter how much money you want to put in: ")
     inputMoney = int(inputMoney)
     if inputMoney < 0:
@@ -41,6 +52,17 @@ def get_user_money():
 def checking_guess(random_num):
     guesses = 0
     global money
+    global hints
+
+    """This new feature will make so if input = y then it will make hints = True and call the function which prints out which number is in the numble"""
+
+    hintsOn = input("Do you want hints? y/n: ")
+    if hintsOn == "y":
+            hints = True
+            get_hints(random_num)
+
+
+
     moneyInput = get_user_money()
     while guesses < 6:
         colour_positions = []
@@ -55,7 +77,7 @@ def checking_guess(random_num):
                 colour_positions.append("⬜")
         print(colour_positions)
         if colour_positions == ['🟩','🟩','🟩','🟩','🟩']:
-            print(f"You win! You guessed in {guesses + 1} guess")
+            print(f"You win! You guessed in {guesses + 1} guess(es)")
             money += moneyInput * 2
             return True  # Win
         guesses += 1
@@ -67,11 +89,11 @@ def check_wins(win):
     global losses
     if win:
         wins += 1
-        print(f"Your money has risen to {money}")
+        print(f"Your money has risen to {money}\n")
     else:
         losses += 1
     print("Total losses: ", losses)
-    print("Total wins:", wins)
+    print("Total wins:", wins, "\n")
     yn = input("Play again? Y/N: ")
     if yn.lower() == "y":
         main()
@@ -82,4 +104,3 @@ def main():
     check_wins(game)
 
 main()
-#63
