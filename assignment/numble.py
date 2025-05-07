ff4f4d5ccccf080dfb5984f2e518a2a9de0cc14d
1lent <brodiecuff@outlook.com>
2025-05-07 09:18:51 +0800

    polishing and comments

--- assignment/numble.py
+++ assignment/numble.py
@@ -5,7 +5,9 @@ losses = 0
 money = 1000
 hints = False
 
-"""Addition for the assignment to add hints, in checking_guess() it will put an input saying do u want hints, if so it will call this once it sets hints = True"""
+"""Addition for the assignment to add hints, in checking_guess(),
+ it will put an input saying do u want hints,
+  if so it will call this once it sets hints = True"""
 
 
 def get_hints(random_num):
@@ -15,6 +17,8 @@ def get_hints(random_num):
 
 
 def get_random_number():
+    """Added This to get random numble from the list of numbers in the txt file.
+    Stupid me should have coded it to get a random number 10000-99999"""
     numbers = []
     with open("five_digit_numbers.txt") as open_file:
         open_file = open_file.readlines()
@@ -26,6 +30,10 @@ def get_random_number():
 
 
 def get_user_number():
+    """Gets user number, when called and returns the users input user_number
+    if number is !=5 recalls the function which is called recursion so they have to redo
+    the input"""
+
     user_number = input("Enter a 5 digit number to guess the numble: ")
     if len(user_number) != 5:
         print("\ntry again\n5 Digits")
@@ -34,6 +42,17 @@ def get_user_number():
         return user_number
 
 def get_user_money():
+    """
+    Added Gambling system i asked carl to playtest my game he said it was alright bit boring so added this
+    What it does is you start with 1000 and when this is called you input the money u want to bet and it takes it from your bal
+    if you guess the number your money will get doubled but thats in another function
+
+    i used recursion again here to recall the function if the user enters -1 money as that was a bug if they bet negative number
+    if they lost they would get the money
+
+    if the money is <0 they would lose and go bankrupt ending the game.
+    I made it < instead of <= so if they didnt want to bet anything.
+    """
     global money
     print("\nYour current amount of money is"  , money, "\n")
     inputMoney = input("Enter how much money you want to put in: ")
@@ -54,30 +73,30 @@ def checking_guess(random_num):
     global money
     global hints
 
-    """This new feature will make so if input = y then it will make hints = True and call the function which prints out which number is in the numble"""
-
-    hintsOn = input("Do you want hints? y/n: ")
+    """This new feature will make so if input = y then it will make hints = True,
+     and call the function which prints out which number is in the numble"""
+    hintsOn = input("\nDo you want hints? y/n: ")
     if hintsOn == "y":
             hints = True
             get_hints(random_num)
 
+    moneyInput = get_user_money() #calls get_user_money which is the gambling function
 
-
-    moneyInput = get_user_money()
-    while guesses < 6:
+    while guesses < 6: #guessing system after 6 guesses game over
         colour_positions = []
         user = get_user_number()
 
         for i in range(len(random_num)):
             if user[i] == random_num[i]:
-                colour_positions.append("🟩")
+                colour_positions.append("🟩") #if in correct spot in array it prints this meaning correct spot
             elif user[i] in random_num:
-                colour_positions.append("🟧")
+                colour_positions.append("🟧") #if in array not rright spot prints this  but means its in diff spot
             else:
-                colour_positions.append("⬜")
+                colour_positions.append("⬜") #not in numble at all
         print(colour_positions)
-        if colour_positions == ['🟩','🟩','🟩','🟩','🟩']:
-            print(f"You win! You guessed in {guesses + 1} guess(es)")
+        if colour_positions == ['🟩','🟩','🟩','🟩','🟩']: #winning if the array is all green with system above you win and money is doubled returning true meaning Win
+
+            print(f"You win! You guessed in {guesses + 1} guess(es)") #guesess goes up so if u win it just doesnt disreguard the guess
             money += moneyInput * 2
             return True  # Win
         guesses += 1
@@ -86,20 +105,25 @@ def checking_guess(random_num):
 
 def check_wins(win):
     global wins
-    global losses
-    if win:
+    global losses #gets updated global variables
+
+    if win: #if win so if checking_guess returned True then does this if statement
         wins += 1
-        print(f"Your money has risen to {money}\n")
-    else:
-        losses += 1
+        print(f"Your money has risen to {money}\n") #shows that money has doubled if win
+    else: #if guessing_guess returned false then does this else
+        losses += 1 #losees count goes up 1
     print("Total losses: ", losses)
     print("Total wins:", wins, "\n")
-    yn = input("Play again? Y/N: ")
+    yn = input("Play again? Y/N: ") #continuous if y or n, if y play again if n exit program
     if yn.lower() == "y":
         main()
     else: exit()
 
+
 def main():
+    """Main function that makes the game working passing get_random_number as the parameter for the checkingguess function
+    then once that returns True or False Check_wins will be called with game as the parameter
+    """
     game = checking_guess(get_random_number())
     check_wins(game)
 
